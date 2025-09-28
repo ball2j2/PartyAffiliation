9604a0dd95ddd33a92be856ff8f9223ad42fc7cc
jordanball <ball2j2@mail.uc.edu>
2025-09-28 18:33:36 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Pseudo code above
+
+        Scanner in = new Scanner(System.in);
+        String choice;
+
+        System.out.print("Enter your party affiliation (D, R, I): ");
+        choice = in.nextLine();
+
+        if (choice.equalsIgnoreCase("D")) {
+            System.out.println("You get a Democratic Donkey.");
+        } else if (choice.equalsIgnoreCase("R")) {
+            System.out.println("You get a Republican Elephant.");
+        } else if (choice.equalsIgnoreCase("I")) {
+            System.out.println("You get an Independent Person.");
+        } else {
+            System.out.println("You get an Other Party.");
+        }
+    }
+}
